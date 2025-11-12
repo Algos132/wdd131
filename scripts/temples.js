315688462b49cfda19f8383054c70d5e8e63c62e
Algos132 <mikeph132@gmail.com>
2025-11-11 17:33:55 -0700

    First draft of temples assignment completed

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,18 @@
 const date = new Date();
 document.querySelector("#currentyear").innerHTML = date.getFullYear();
 document.getElementById("lastmodified").innerHTML = `Last Modification: ${document.lastModified}`;
+
+const hambutton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+
+hambutton.addEventListener("resize", () => {
+    if (window.innerWidth >= 768){
+        navigation.classList.remove('show');
+        hambutton.classList.remove('show');
+    }     
+});
