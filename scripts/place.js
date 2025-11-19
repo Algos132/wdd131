ecfb18d93a961bab9a7e17180fbeddfb1518172b
Algos132 <mikeph132@gmail.com>
2025-11-18 17:47:35 -0700

    Completed large form for Country Page assignment

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,5 @@
+//Dates for footer element
 const date = new Date();
 document.querySelector("#currentyear").innerHTML = date.getFullYear();
 document.getElementById("lastmodified").innerHTML = `Last Modification: ${document.lastModified}`;
+
