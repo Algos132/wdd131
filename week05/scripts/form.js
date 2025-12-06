2954a6ba913d8a9ae28349f0da704d745210fcf6
Algos132 <mikeph132@gmail.com>
2025-12-05 17:20:01 -0700

    Made progress on Product Review form

---
+++ week05/scripts/form.js
@@ -0,0 +1,63 @@
+/*Gets date for footer*/
+const date = new Date();
+document.querySelector("#currentyear").innerHTML = date.getFullYear();
+document.getElementById("lastmodified").innerHTML = `Last Modification: ${document.lastModified}`;
+
+/*For the form*/
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
+];
+
+/*Populate dropdown*/
+const select = document.querySelector("#product");
+
+if (select) {
+    products.forEach(product => {
+        let dropdownItem = document.createElement('option');
+        dropdownItem.value = product.id;
+        dropdownItem.textContent = product.name;
+
+        select.appendChild(dropdownItem);
+    });
+}
+
+/*Visit Counter on confirmation page*/
+const main = document.querySelector("confirmation");
+
+console.log(main);
+
+if (main) {
+    let counter = document.createElement('p');
+    let visitNumber = Number(window.localStorage.getItem("times-visited")) || 0;
+
+
+    main.appendChild(counter)
+    visitNumber++;
+    localStorage.setItem("times-visited", visitNumber);
+
+    counter.textContent = `You have submitted ${visitNumber} reviews!`;
+}
